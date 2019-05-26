2f432766acca09f37c315094f239ecc0f18c345c
evandanielsebastian <eds@evandaniel.com>
2019-05-26 10:09:53 -0700

    format

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ import * as router from '/router.js';
                 
 window.addEventListener('popstate', windowPopstate => {
     const state = windowPopstate.state; 
-    console.log(state); 
+
+
 
     document.querySelector('.content').innerHTML = state.content; 
 }); 
